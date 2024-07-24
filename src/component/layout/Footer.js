e662dbf9c8a161ea42d15b0fe9f2d56ff7fce2a9
zueunjoeng <judms0314@gmail.com>
2024-07-24 18:05:50 +0900

    [1.1.6] 하단 작업, 가로스크롤 제거

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react'
 import ft from '../../scss/layout/footer.module.scss';
+import { Link } from 'react-router-dom';
 
 
 function Footer() {
     return (
-       <footer>하단</footer>
+       <footer className='border-top'>
+        <div className='d-flex align-items-center'>
+        <Link to="/">
+            <img src="./img/tving logo.png" alt="티빙 로고" />
+        </Link>
+        <p className='ms-auto'>고객센터    이용약관    개인정보처리방침    청소년    보호정책    법적고지    이벤트    인재채용</p>
+       </div>
+       </footer>
     )
 }
 
