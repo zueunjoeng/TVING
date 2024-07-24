e2b164908d385ce46d3d66a0ca542e7033cb8828
zueunjoeng <judms0314@gmail.com>
2024-07-24 17:38:38 +0900

    [1.1.5] 콘텐츠 작업

--- src/component/main/Faq.js
+++ src/component/main/Faq.js
@@ -1,13 +1,49 @@
-import React, { useState, useEffect, useRef } from 'react'
-import faq from '../../scss/layout/faq.module.scss';
+import React, { useState } from 'react';
+import faq from '../../scss/layout/faq.module.scss'; // CSS 모듈 가져오기
+import faqlistData from '../../data/faqDB.json';
 
+function Faq(props) {
+  const [faqmenu, setFaqmenu] = useState(0);
 
-function Faq() {
-    return (
-       <section>
-        <p className='titleText'>FAQ</p>
-       </section>
-    )
+  const qnaclick = (num) => {
+    setFaqmenu(num);
+  };
+  
+
+  const { faqlist } = faqlistData;
+
+  return (
+    <section>
+      <p className='titleText'>
+        FAQ
+      </p>
+      <div id={props.keynm} className="d-flex flex-wrap flex-row">
+        {
+          faqlist.map((e, i) => (
+            <button
+              key={`category${i}`}
+              className={`${faq.faqCategory} ${faqmenu === i ? faq.on : ''}`}
+              onClick={() => qnaclick(i)}
+            >
+              {e.name}
+            </button>
+          ))
+        }
+      </div>
+      <div id="faq_list">
+        {
+          faqlist[faqmenu].list.split('|').map((v, i) => (
+            <p key={`faq-item${i}`}>
+              {v}
+            </p>
+          ))
+        }
+      </div>
+    </section>
+    
+  );
+  
 }
 
-export default Faq
+export default Faq;
+
--- src/component/main/watching.js
+++ src/component/main/watching.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import watching from '../../scss/layout/watching.module.scss'
+import React, { useEffect } from 'react';
+import watching from '../../scss/layout/watching.module.scss';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
 function Watching() {
-    return (
-      <section>
-      <p className='titleText'>내가 원하는 기기로 자유롭게<br/>언제 어디서든 본방사수! </p>
-     </section>
-    )
+  useEffect(() => {
+    AOS.init({
+      duration: 1500, // 애니메이션 지속 시간
+    });
+  }, []);
+
+  return (
+    <section>
+      <p className='titleText'>
+        내가 원하는 기기로 자유롭게<br />언제 어디서든 본방사수!
+      </p>
+      <div className='container d-flex'>
+        <div className='col-5' data-aos="fade-up">
+          <img src="/img/consoleimg/pcmedia.png" alt="PC Media" />
+          <p className='hashTitle mt-4'>
+            #스마트폰 #테블릿 #PC #TV<br />
+            #최대 <span className={watching.txt}>4인 멀티 프로필</span>
+          </p>
+        </div>
+        <div className='col-2'></div>
+        <div className='col-5' data-aos="fade-up">
+          <img src="/img/consoleimg/scence.png" alt="Scene" />
+          <p className='hashTitle mt-4'>
+            #<span className={watching.txt}>Quick VOD</span><br />
+            #놓친 방송은 타임머신으로 다시보기!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Watching
+export default Watching;
