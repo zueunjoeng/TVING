86c97e6be70b14a20a1b2fae0246e607aa0e8e17
zueunjoeng <judms0314@gmail.com>
2024-09-12 15:11:23 +0900

    스와이퍼 버전 업데이트

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import ft from '../../scss/layout/footer.module.scss';
 import { Link } from 'react-router-dom';
 
